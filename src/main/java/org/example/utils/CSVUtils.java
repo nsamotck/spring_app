123a0082353b04d6330366590c9b78420b55912b
Nick Samotsky <n.samotskiy@gmail.com>
2024-04-17 15:30:58 +0300

    read from csv method created

--- src/main/java/org/example/utils/CSVUtils.java
+++ src/main/java/org/example/utils/CSVUtils.java
@@ -1,13 +1,20 @@
 package org.example.utils;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import com.opencsv.bean.StatefulBeanToCsv;
 import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import org.example.contract.CSVBean;
 
 import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Reader;
 import java.io.Writer;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CSVUtils {
 
@@ -28,5 +35,24 @@ public class CSVUtils {
                     .build();
             beanWriter.write(beans);
         }
+    }
+
+    /***
+     * Returns a Set of java beans read from a csv-file
+     * @param path - path to csv
+     * @param clazz - type of bean
+     * @return - Set of beans read from a file
+     * @param <T> - type parameter for bean class
+     * @throws IOException - if reading failed
+     */
+    public static <T extends CSVBean> Set<T> readBeansFromCSV(Path path, Class<T> clazz) throws IOException {
+        CsvToBean<T> cb;
+        try (Reader reader = Files.newBufferedReader(path)) {
+            cb = new CsvToBeanBuilder<T>(reader)
+                    .withSeparator(';')
+                    .withType(clazz)
+                    .build();
+        }
+        return new HashSet<>(cb.parse());
     }
 }
